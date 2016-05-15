9fc634ca5e8ace898f4f46c8159892b604b7cac3
Parvez shaikh <parvezshaikhj@gmail.com>
2016-05-15 13:39:33 +0530

    Minor css and js fixes

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,42 +4,34 @@ $(document).ready(function() {
 
 	    if (wScroll >= 400) {
 	        $(".nav-bar").addClass("sticky-nav");
-	        $("section.education").css("padding-top", "75px");
 	    } else {
 	        $(".nav-bar").removeClass("sticky-nav");
-	        $("section.education").css("padding-top", "0px");
 	    }
 	});
 
 	$("nav a").on("click", function(){
 		$this = $(this);
 		var anchorVal = $this.text().toLowerCase();
-		//console.log(anchorVal);
 		if(anchorVal === "education") {
 			$('html, body').animate({
 	        scrollTop: $('#'+ anchorVal +'').offset().top
 	    }, 500);
-	    $('#'+ anchorVal +'').css("padding-top","75px");
 		} else if(anchorVal === "expertise") {
 			$('html, body').animate({
 	        scrollTop: $('#'+ anchorVal +'').offset().top
 	    }, 500);
-	    $('#'+ anchorVal +'').css("padding-top","75px");
 		} else if(anchorVal === "experience") {
 			$('html, body').animate({
 	        scrollTop: $('#'+ anchorVal +'').offset().top
 	    }, 500);
-	    $('#'+ anchorVal +'').css("padding-top","75px");
 		} else if(anchorVal === "project") {
 			$('html, body').animate({
 	        scrollTop: $('#'+ anchorVal +'').offset().top
 	    }, 500);
-	    $('#'+ anchorVal +'').css("padding-top","75px");
 		} else if(anchorVal === "contact") {
 			$('html, body').animate({
 	        scrollTop: $('#'+ anchorVal +'').offset().top
 	    }, 500);
-	    $('#'+ anchorVal +'').css("padding-top","75px");
 		} else {
 			return false;
 		}
